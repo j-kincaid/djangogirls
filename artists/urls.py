c2e495af09f80de0079e0e823b0da9a379322e6d
Jessica Kincaid <4j.kincaid@gmail.com>
2021-10-11 13:54:34 -0500

    move artists, panelists, submissions to src curate

--- artists/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('artists/<str:pk>/', views.artists, name='artists'),
-    path('artist/<str:pk>/', views.artist, name='artist'),
-]
